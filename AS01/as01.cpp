281ecf80ab450f0fc97e6e83948681fa762ed8fe
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-08-04 22:14:26 -0300

    terminado AS01

---
+++ AS01/as01.cpp
@@ -0,0 +1,47 @@
+//AS01
+//Autor: Gustavo Lopes Rodrigues
+#include<iostream>
+#include<string>
+
+#define MIN 0
+#define MAX 20
+
+int isInInterval(int number);
+int stringToInt(std::string input, int firstIndex, int lastIndex );
+int fatorial(int n);
+
+int main() {
+
+    std::string input;
+  
+    //Fazer a leitura da entrada do teclado, enquanto o mesmo
+    //não for vazia
+    while (std::getline(std::cin, input) && !input.empty()) {
+
+        //Resgatar numeros da entrada do usuario
+        int m = stringToInt(input,0,input.find(" "));
+        int n = stringToInt(input,input.find(" "),input.length() -1);
+
+        if(isInInterval(m) && isInInterval(n))
+            std::cout<< fatorial(m) + fatorial(n) << std::endl;
+    }
+
+}
+
+// Converter um intervalo de uma std::string para um numero inteiro(int)
+int stringToInt(std::string input, int firstIndex, int lastIndex ) {
+    return std::stoi(input.substr(firstIndex,lastIndex));
+}
+
+// Verificar se numero esta dentro do intervalo
+int isInInterval(int number) {
+    return (number >= MIN && number <= MAX);
+}
+
+// Retornar o resultado do fatorial do número 'n'
+int fatorial(int n) {
+    if(n == 0)
+        return 1;
+    
+    return n * fatorial(n-1); 
+}

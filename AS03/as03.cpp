621f1f834f9edef18e50fc3117e0e70cd04d6dbb
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-08-17 21:11:08 -0300

    feat: terminado questao AS03

--- AS03/as03.cpp
+++ AS03/as03.cpp
@@ -1,19 +1,22 @@
+//AS03: Gustavo Lopes Rodrigues 
 #include<iostream>
 #include<string>
 
 bool is_inside_input(std::string input, std::string query);
 
 int main() {
-    int n;
-    
-    std::string input;
+
+    std::string input; // string entrada para procura de query
+    int n;             // numero de 'std::string input' que sera lido
 
     std::cin >> n;
 
     for(int i = 0; i < n; i++) {
-
+        
         int nQueries;
 
+        // Inserir o 'input' e fazer a leitura de quantas query
+        // sera lido
         std::cin >> input;
         std::cin >> nQueries;
         for(int j = 0; j < nQueries; j++) {
@@ -38,6 +41,24 @@ int main() {
     return 0;
 }
 
+// Funcao para verificar se uma string 'query' encontra-se
+// dentro da string 'input' de forma nao sequencial
 bool is_inside_input(std::string input, std::string query) {
-    return input.find(query) != -1;
+    
+    int query_index =0;
+
+    for(int i = 0; i < input.size(); i++) {
+
+        if(input.at(i) == query.at(query_index)) 
+            query_index++;
+
+        //Se o query index for igual ao tamanho dela
+        //significa que foi possivel encontrar todos
+        //os caracteres da query dentro do input,
+        //logo a operacao foi um sucesso
+        if(query_index == query.size())
+            return true;
+    }
+
+    return false;
 }

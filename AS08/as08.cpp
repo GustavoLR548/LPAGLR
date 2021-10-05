f3031232bfb3f209ba7641561d41174fca783810
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-10-05 09:25:19 -0300

    feat: adicionado comentarios

--- AS08/as08.cpp
+++ AS08/as08.cpp
@@ -1,6 +1,7 @@
 //AS08: Gustavo Lopes Rodrigues
 #include <iostream>
 #include <vector>
+#include <string>
 
 /*
 Complexidade: O(n*log(n)) O jogo de bolhas de baldes consiste em realizar um algoritmo de 
@@ -8,32 +9,38 @@ Divisão e conquista. Para a resolução do problema, foi utilizado o algoritmo 
 logo, o preço do algoritmo é o preço do mergesort
 */
 
-//Classe para gerenciamento do jogo
+typedef std::vector<int> numeros;
+typedef std::vector<std::string> jogadores;
+
+//Classe para gerenciamento do jogo de Bolhas e Baldes
 class BolhasBaldes {
 
     private:
         
         // Fazer a juncao da esquerda e da direita
-        std::vector<int> merge(std::vector<int> esq, std::vector<int> dir) {
+        std::vector<int> merge(numeros esq, numeros dir) {
             
             int i = 0;
             int j = 0;
 
-            std::vector<int> juncao;
+            numeros juncao;
 
             while (i < esq.size() && j < dir.size()) {
 
                 if (esq[i] < dir[j]) {
                     juncao.push_back(esq[i++]);
 
-                } else { // troca dos números, logo ,+ numero de jogadas
+                } else { // esq[i] > dir[j] 
                     juncao.push_back(dir[j++]);
-                    num_jogadas += esq.size() - i;
+                    num_jogadas += esq.size() - i; // adicionar numeros de jogadas feitas a variavel
                 }
             }
 
+            // Adicionar numeros restantes na juncao,
+            // o i e o j nao sejam tamanho dos vetores que estao associados
+
             while (i  < esq.size() ) 
-                juncao.push_back(esq [i++ ]);
+                juncao.push_back(esq[i++]);
 
             while (j < dir.size()) 
                 juncao.push_back(dir[j++]);
@@ -42,16 +49,16 @@ class BolhasBaldes {
         }
 
         // Iniciando o algoritmo de mergesort
-        std::vector<int> mergeSort(std::vector<int>& arr) {
+        std::vector<int> mergeSort(numeros& a) {
 
-            if (arr.size() <= 1)
-                return arr;
+            if (a.size() <= 1)
+                return a;
 
-            int tam = arr.size() / 2;
+            int metade = a.size() / 2;
 
             // Fazer divisao do vetor
-            std::vector<int> esq (arr.begin(),arr.begin() + tam);
-            std::vector<int> dir(arr.begin() + tam, arr.end());
+            std::vector<int> esq (a.begin(),a.begin() + metade);
+            std::vector<int> dir(a.begin() + metade, a.end());
 
             // Fazer a juncao dos segmentos da esquerda e direita
             return merge(mergeSort(esq), mergeSort(dir));
@@ -62,45 +69,51 @@ class BolhasBaldes {
         // Numero de jogadas realizadas
         int num_jogadas;
 
-        // Guardando os numeros aleatorios
-        std::vector<int> numeros;
-
-        BolhasBaldes(std::vector<int> array) {
-            this->numeros     = array;
-            this->num_jogadas = 0;
+        // Jogar o jogo de Bolhas e baldes, usando o vetor
+        // de numeros como base
+        void jogar(std::vector<int> numeros) {
+            num_jogadas = 0;
+            this->mergeSort(numeros);
         }
 
-        // Funcao para ser chamada, ao jogar o jogo
-        void jogar() {
-            num_jogadas = 0;
-            this->mergeSort(this->numeros);
+        // Funcao para calcular o vencedor do jogo
+        std::string vencedor(jogadores j) {
+
+            // O vencedor do jogo, é o jogador que ira jogar o proximo
+            // turno, depois do ultima jogada
+            int indice_vencedor = num_jogadas % j.size();
+
+            return j[indice_vencedor];
         }
 
 };
 
 int main() {
 
-    int n;
+    int tam;
+    BolhasBaldes bolhasEbaldes;
+    jogadores participantes = {"Carlos", "Marcelo"};
 
-    std::cin >> n;
+    std::cin >> tam;
 
-    while (n != 0)
+    //Leitura das jogadas
+    while (tam != 0)
     {
 
-        std::vector<int> numeros;
+        numeros nums;
 
-        for (int i = 0; i < n; i++)
+        // Leitura dos numeros do jogo
+        for (int i = 0; i < tam; i++)
         {
             int tmp;
             std::cin >> tmp;
-            numeros.push_back(tmp);
+            nums.push_back(tmp);
         }
 
-        BolhasBaldes jogo(numeros);
-        jogo.jogar();
+        //Jogar o jogo do bolhas e baldes e imprimir o vencedor
+        bolhasEbaldes.jogar(nums);
+        std::cout << bolhasEbaldes.vencedor(participantes) << std::endl;
 
-        std::cout << ((jogo.num_jogadas % 2 == 0) ? "Carlos" : "Marcelo") << std::endl;
-
-        std::cin >> n;
+        std::cin >> tam;
     }
 }
